15086fb08fdd44791dae61f4944f5e94715767b2
Tristan A. <tristanagraa@gmail.com>
2025-03-28 18:22:14 +0700

    chore: init springboot project

---
+++ src/main/java/com/example/coursebe/CourseBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursebe/CourseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
